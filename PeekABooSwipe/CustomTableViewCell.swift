73f2c2faf18ac7967331c5d70a2eeebb0f201930
Mike Finney <mikefinney@carfax.com>
2019-04-12 13:57:30 -0500

    put solution in by "Hobbes the Tige"

--- PeekABooSwipe/CustomTableViewCell.swift
+++ PeekABooSwipe/CustomTableViewCell.swift
@@ -7,24 +7,52 @@ class CustomTableViewCell: UITableViewCell {
         // Initialization code
     }
 
+//    func applyCellAnimations() {
+//
+////        self.contentView.translatesAutoresizingMaskIntoConstraints = false
+//
+//        self.contentView.transform = .identity
+//
+//        UIView.animate(
+//            withDuration: 1,
+//            delay: 1,
+//            options: [.curveEaseInOut],
+//            animations: {
+//                self.contentView.transform = CGAffineTransform(translationX: 1000, y: 0)
+//                self.backgroundColor = UIColor.green
+//        }, completion: { _ in
+//            self.contentView.transform = .identity
+//        })
+//
+//
+//        print("applyCellAnimations")
+//    }
+    
+    // solution by https://stackoverflow.com/users/674205/hobbes-the-tige
     func applyCellAnimations() {
-
-//        self.contentView.translatesAutoresizingMaskIntoConstraints = false
-
-        self.contentView.transform = .identity
-
+        
+        let originalCenter = contentView.center
+        let offsetCenter = originalCenter.applying(.init(translationX: -44, y: 0))
+        
+        animateToCenter(offsetCenter) {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1, execute: {
+                self.animateToCenter(originalCenter)
+            })
+        }
+    }
+    
+    private func animateToCenter(_ center: CGPoint, completionHandler: @escaping () -> Void = { }) {
+        
+        layoutIfNeeded()
+        
         UIView.animate(
             withDuration: 1,
-            delay: 1,
+            delay: 0,
             options: [.curveEaseInOut],
             animations: {
-                self.contentView.transform = CGAffineTransform(translationX: 1000, y: 0)
-                self.backgroundColor = UIColor.green
-        }, completion: { _ in
-            self.contentView.transform = .identity
+                self.contentView.center = center
+        }, completion: { didComplete in
+            if didComplete { completionHandler() }
         })
-
-        
-        print("applyCellAnimations")
     }
 }
